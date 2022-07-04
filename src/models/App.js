5c4d2302b5d4287ec8502b7847f2dd14c0bb66bd
Pjota94 <juniorgurinhem9@gmail.com>
2022-07-04 11:37:24 -0300

    Feat:Concluido

--- src/models/App.js
+++ src/models/App.js
@@ -12,18 +12,19 @@ export class App{
     static async requisicaoGet(){
         const result = await this.kenzieNewsGet()
         result.forEach((elem)=>{
-            App.templateDesktop(elem.imagem,elem.categoria,elem.titulo,elem.resumo,elem.fonte);
-            App.templateMobile(elem.categoria,elem.titulo,elem.resumo,elem.fonte);
+            App.templateDesktop(elem.imagem,elem.categoria,elem.noticia_completa,elem.titulo,elem.resumo,elem.fonte);
+            App.templateMobile(elem.categoria,elem.noticia_completa,elem.titulo,elem.resumo,elem.fonte);
         })
     }
 
-    static templateDesktop(imagem,categoria,titulo,resumo,fonte){
+    static templateDesktop(imagem,categoria,link,titulo,resumo,fonte){
         const selectArticleDesktop = document.querySelector('#article-desktop')
         //Criando elements
         const createDivPai    = document.createElement('div');
         const createDivImg    = document.createElement('div');
         const createImg       = document.createElement('img');
         const createDivTexts  = document.createElement('div');
+        const createLink      = document.createElement('a');
         const createTag       = document.createElement('p');
         const createTittle    = document.createElement('p');
         const createSubtittle = document.createElement('p');
@@ -34,7 +35,8 @@ export class App{
         createImg.classList.add("img-article-desktop")
         createDivTexts.classList.add("texts-article-desktop")
         createTag.classList.add("tag-article-desktop")
-        createTittle.classList.add("tittle-article-desktop")
+        createLink.classList.add("tittle-article-desktop")
+        createTittle.classList.add('textoLinkArticle')
         createSubtittle.classList.add("subtittle-article-desktop")
         createFont.classList.add("font-article-desktop")
         //Inserindo conteudo
@@ -43,24 +45,30 @@ export class App{
         createTittle.innerText = titulo;
         createSubtittle.innerText = resumo;
         createFont.innerText = fonte;
+        //Inserindo conteudo Link
+        createLink.href = link;
+        createLink.target = '_blank'
         //Inserindo elements
         createDivImg.appendChild(createImg);
-        createDivTexts.append(createTag,createTittle,createSubtittle,createFont)
+        createLink.appendChild(createTittle)
+        createDivTexts.append(createTag,createLink,createSubtittle,createFont)
         createDivPai.append(createDivImg,createDivTexts)
         selectArticleDesktop.appendChild(createDivPai)
     }
 
-    static templateMobile(categoria,titulo,resumo,fonte){
+    static templateMobile(categoria,link,titulo,resumo,fonte){
         const selectArticleMobile = document.querySelector('#article-mobile');
         //Criando elements
         const createDivPai    = document.createElement('div');
         const createTag       = document.createElement('p');
+        const createLink      = document.createElement('a');
         const createTittle    = document.createElement('p');
         const createSubtittle = document.createElement('p');
         const createFont      = document.createElement('p');
         //Adicionando Class
         createDivPai.classList.add("card-article")
         createTag.classList.add("tag-article")
+        createLink.classList.add("link-article-mobile")
         createTittle.classList.add("tittle-article")
         createSubtittle.classList.add("subtittle-article")
         createFont.classList.add("fonte-article")
@@ -69,8 +77,11 @@ export class App{
         createTittle.innerText = titulo;
         createSubtittle.innerText = resumo;
         createFont.innerText = fonte;
+        createLink.href = link;
+        createLink.target = '_blank'
         //Inserindo elements
-        createDivPai.append(createTag,createTittle,createSubtittle,createFont)
+        createLink.appendChild(createTittle)
+        createDivPai.append(createTag,createLink,createSubtittle,createFont)
         selectArticleMobile.appendChild(createDivPai)
     }
 }
